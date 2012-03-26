9d472f163c11b56df1de8a0cd55d1daf3b6bb812
Isaac <hyperneato@gmail.com>
2012-03-26 17:48:38 -0400

    working on finding neighbors

--- gol.py
+++ gol.py
@@ -7,32 +7,45 @@
 
 
 def create_2x2_matrix(r, c):
-    
     return [[0 for i in range(r)] for j in range(c)]
 
-m = create_2x2_matrix(10, 10)
+matrix = create_2x2_matrix(10, 10)
 
+matrix[4][3] = 1
 
-m[4][3] = 1
-
-
-    
-
-def wrap(cell, dimension):
-    if (cell > dimension) or (cell < 0):
-        return cell % dimension
+def wrap(r, c, matrix):
+    row = None
+    col = None
+    if (r > (len(matrix) - 1)) or (r < 0):
+        row = r % len(matrix)
     else:
-        return cell
-
+        row = r
+    if (c > (len(matrix) - 1)) or (c < 0):
+        col = c % len(matrix[0])
+    else:
+        col = c
+    return (row, col)
 
 def check_neighbors(r, c, matrix):
     neighbors = []
-    for row_i in range(len(matrix[0])): # each row in matrix
-        for col_i in range(len(matrix[0][0])) : # each column
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for row_i in range(rows): # each row in matrix
+        for col_i in range(cols) : # each column
             for row_check in (-1, 0, 1):
                 for col_check in (-1, 0, 1):
-                    if matrix[wrap(r + row_check, len(matrix)][wrap(c + row_check)] == 1
-                    
-check_neighbors(m)
+                    if (row_check == 0) and (col_check == 0):
+                        pass
+                    else:
+                        cell_row, cell_col = wrap((r + row_check), (c + col_check),  matrix)
+                        if matrix[cell_row][cell_col] == 1:
+                            neighbors.append([cell_row, cell_col])
+            if len(neighbors) > 0:
+                print("cell has neighbor")
+ 
+for row in range(len(matrix)):
+    for cell in range(len(matrix[0])):
+        check_neighbors(row, cell, matrix)
+
                     
         
