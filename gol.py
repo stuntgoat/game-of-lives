cf99bf830b2ba1f3bc18466de67195bb54aa4949
Isaac <hyperneato@gmail.com>
2012-03-26 23:43:40 -0400

    860ish game @ 45x45

--- gol.py
+++ gol.py
@@ -2,18 +2,60 @@
 from time import sleep
 import sys
 
-R = 10
-C = 10
+R = 45
+C = 45
 
 def create_matrix(r, c):
     return [[0 for i in range(r)] for j in range(c)]
 
 matrix = create_matrix(R, C)
-matrix[4][3] = 1
-matrix[4][4] = 1
-matrix[3][3] = 1
-matrix[3][4] = 1
-matrix[2][4] = 1
+
+matrix[0][2] = 1
+matrix[1][0] = 1
+matrix[1][2] = 1
+matrix[2][1] = 1
+matrix[2][2] = 1
+matrix[11][2] = 1
+matrix[12][0] = 1
+matrix[12][2] = 1
+matrix[13][1] = 1
+matrix[13][2] = 1
+matrix[7][5] = 1
+matrix[8][5] = 1
+matrix[8][7] = 1
+matrix[9][5] = 1
+matrix[9][6] = 1
+matrix[33][2] = 1
+matrix[33][3] = 1
+matrix[33][4] = 1
+matrix[32][4] = 1
+matrix[33][3] = 1
+matrix[15][14] = 1
+matrix[14][14] = 1
+matrix[13][14] = 1
+matrix[12][14] = 1
+matrix[11][14] = 1
+matrix[10][14] = 1
+matrix[24][2] = 1
+matrix[24][3] = 1
+matrix[24][4] = 1
+matrix[23][4] = 1
+matrix[22][3] = 1
+matrix[24][2] = 1
+matrix[24][3] = 1
+matrix[24][4] = 1
+matrix[23][4] = 1
+matrix[22][3] = 1
+matrix[41][2] = 1
+matrix[41][3] = 1
+matrix[41][4] = 1
+matrix[40][4] = 1
+matrix[39][3] = 1
+
+
+
+
+
 
 
 def wrap(r, c, R=R, C=C):
@@ -47,16 +89,23 @@ def generate_next(matrix):
             elif count == 3:
                 next[row][cell] = 1
     return next
+count = 0
 while 1:
-    for line in matrix:
-        print("%s \r" % line)
 
-    sys.stdout.flush()
+    for line in matrix:
+        
+        sys.stdout.write("%s \n" % line)
+#        sys.stdout.write("\r" % line)
+        sys.stdout.seek(0)
+        #sys.stdout.flush()
+    sys.stdout.write('%d \n' % count)
+    
     sys.stdout.seek(0)
     
     matrix = generate_next(matrix)
-    sleep(1)
-                
+    sleep(.01)
+    count += 1
+    
             
 
 # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
