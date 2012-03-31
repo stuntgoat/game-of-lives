054b032b4947d5860e02b12272cd68a720f8ed9c
music <music@social.local>
2012-03-31 01:57:15 -0400

    2 speeds working!! but blocking and not breeding

--- gol.py
+++ gol.py
@@ -14,64 +14,63 @@ matrix[1][0]   ='O'
 matrix[1][2]   ='O'
 matrix[2][1]   ='O'
 matrix[2][2]   ='O'
-matrix[11][2]  ='O'
-matrix[12][0]  ='O'
-matrix[12][2]  ='O'
-matrix[13][1]  ='O'
-matrix[13][2]  ='O'
-matrix[7][5]   ='O'
-matrix[8][5]   ='O'
-matrix[8][7]   ='O'
-matrix[9][5]   ='O'
-matrix[9][6]   ='O'
+matrix[11][2]  ='X'
+matrix[12][0]  ='X'
+matrix[12][2]  ='X'
+matrix[13][1]  ='X'
+matrix[13][2]  ='X'
+matrix[7][5]   ='X'
+matrix[8][5]   ='X'
+matrix[8][7]   ='X'
+matrix[9][5]   ='X'
+matrix[9][6]   ='X'
 matrix[33][2]  ='X'
 matrix[33][3]  ='X'
 matrix[33][4]  ='X'
 matrix[32][4]  ='X'
 matrix[33][3]  ='X'
-matrix[15][14] ='O'
-matrix[14][14] ='O'
-matrix[13][14] ='O'
-matrix[12][14] ='O'
-matrix[11][14] ='O'
-matrix[10][14] ='O'
-matrix[24][2]  ='X'
-matrix[24][3]  ='X'
-matrix[24][4]  ='X'
-matrix[23][4]  ='X'
-matrix[22][3]  ='X'
-matrix[24][2]  ='X'
-matrix[24][3]  ='X'
-matrix[24][4]  ='X'
-matrix[23][4]  ='X'
-matrix[22][3]  ='X'
-matrix[41][2]  ='X'
-matrix[41][3]  ='X'
-matrix[41][4]  ='X'
-matrix[40][4]  ='X'
-matrix[39][3]  ='X'
-matrix[41][20]  ='O'
-matrix[41][21]  ='O'
-matrix[41][22]  ='O'
-matrix[40][22]  ='O'
-matrix[39][21]  ='O'
+matrix[15][14] ='X'
+matrix[14][14] ='X'
+matrix[13][14] ='X'
+matrix[12][14] ='X'
+matrix[11][14] ='X'
+matrix[10][14] ='X'
+matrix[24][2]  ='O'
+matrix[24][3]  ='O'
+matrix[24][4]  ='O'
+matrix[23][4]  ='O'
+matrix[22][3]  ='O'
+matrix[24][2]  ='O'
+matrix[24][3]  ='O'
+matrix[24][4]  ='O'
+matrix[23][4]  ='O'
+matrix[22][3]  ='O'
+matrix[41][2]  ='O'
+matrix[41][3]  ='O'
+matrix[41][4]  ='O'
+matrix[40][4]  ='O'
+matrix[39][3]  ='O'
+matrix[41][20]  ='X'
+matrix[41][21]  ='X'
+matrix[41][22]  ='X'
+matrix[40][22]  ='X'
+matrix[39][21]  ='X'
 
-matrix[40][0]  ='X'
-matrix[40][1]  ='X'
-matrix[40][4]  ='X'
-matrix[40][5]  ='X'
-matrix[40][6]  ='X'
-matrix[40][7]  ='X'
-matrix[40][8]  ='X'
-matrix[40][9]  ='X'
-matrix[40][12]  ='X'
-matrix[40][13]  ='X'
-matrix[40][14]  ='X'
-matrix[40][16]  ='X'
-matrix[40][17]  ='X'
-matrix[40][18]  ='X'
-matrix[40][19]  ='X'
-
+matrix[40][0]  ='O'
+matrix[40][1]  ='O'
+matrix[40][4]  ='O'
+matrix[40][5]  ='O'
+matrix[40][6]  ='O'
+matrix[40][7]  ='O'
+matrix[40][8]  ='O'
+matrix[40][9]  ='O'
+matrix[40][12] ='O'
+matrix[40][13] ='O'
+matrix[40][14] ='O'
+matrix[40][16] ='O'
+matrix[40][17] ='O'
+matrix[40][18] ='O'
+matrix[40][19] ='O'
 
 
 
@@ -103,42 +102,38 @@ def generate_next_X(matrix):
         for cell in range(len(matrix[0])):
             count = count_neighbors(row, cell, matrix)
             current = matrix[row][cell]
+            
             if current in ['X']: # HA! substitute `not in` for `in` here
-                if count['sum'] < 2:
+                if count['x'] < 2:
                     next[row][cell] = ' '
-                elif count['sum'] in [2, 3]:
+                elif count['x'] in [2, 3]:
                     next[row][cell] = current
-                elif count['sum'] < 3:
+                elif count['x'] < 3:
                     next[row][cell] = ' '
-            elif current in ['O']: # current is occupied
+            elif count['x'] == 3: # empty cell and exactly 3 neighbors
+                next[row][cell] = 'X'
+            elif current in ['O']:
                 next[row][cell] = current
-            elif count['sum'] == 3: # empty cell and exactly 3 neighbors
-                if count['x'] > count['o']:
-                    next[row][cell] = 'X'
-                else:
-                    next[row][cell] = 'O'
     return next
 
 def generate_next_O(matrix):
     next = create_matrix(len(matrix), len(matrix[0]))
     for row in range(len(matrix)):
         for cell in range(len(matrix[0])):
+            
             count = count_neighbors(row, cell, matrix)
-            last = matrix[row][cell]
-            if last in ['O']: # HA! substitute `not in` for `in` here
-                if count['sum'] < 2:
+            current = matrix[row][cell]
+            if current in ['O']: # HA! substitute `not in` for `in` here
+                if count['o'] < 2:
                     next[row][cell] = ' '
-                elif count['sum'] in [2, 3]:
-                    next[row][cell] = last
-                elif count['sum'] < 3:
+                elif count['o'] in [2, 3]:
+                    next[row][cell] = current
+                elif count['o'] < 3:
                     next[row][cell] = ' '
-            elif last in ['X']:
-                next[row][cell] = last
-            elif count['sum'] == 3:
-                if count['x'] > count['o']:
-                    next[row][cell] = 'X'
-                else:
-                    next[row][cell] = 'O'
+            elif count['o'] == 3:
+                next[row][cell] = 'O'
+            elif current in ['X']:
+                next[row][cell] = current
     return next
 
 def show_matrix(matrix, count1, count2):
@@ -152,16 +147,14 @@ while 1:
     show_matrix(matrix, count1, count2)
     matrix = generate_next_X(matrix)
     count1 += 1
-    sleep(.06)
+    sleep(.04)
 
     show_matrix(matrix, count1, count2)
     matrix = generate_next_O(matrix)
     count2 += 1 
-    sleep(.06)
+    sleep(.04)
 
     show_matrix(matrix, count1, count2)
     matrix = generate_next_O(matrix)
     count2 += 1 
-    sleep(.06)
-
-        
+    sleep(.04)
