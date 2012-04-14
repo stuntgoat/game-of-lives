2808ab557c934c7198d9ce5f858c49c2840d8eea
music <music@social.local>
2012-04-14 01:31:54 -0400

    Factored out seeding of matrix. Added README

---
+++ README.md
@@ -0,0 +1,9 @@
+**Game of Lives**
+
+Based on [Conrad's Game of Life](http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life "Game of Life on Wikipedia").
+
+Long term goals for this project are to have multiple states running on the same matrix. I am thinking of a way that so the cells that appear will inherit characteristics from the 'parent' cells( those that were nearby to create them ).
+
+    $ python gol.py
+
+The above command will create a Game of Life a 45 x 45 matrix with 'X' and 'O' running at 2 speeds.
--- gol.py
+++ gol.py
@@ -2,85 +2,12 @@
 from time import sleep
 import sys
 
-R = 45
-C = 45
-
-def create_matrix(r, c):
-    return [[' ' for i in range(r)] for j in range(c)]
-matrix = create_matrix(R, C)
-
-matrix[0][2]   ='O'
-matrix[1][0]   ='O'
-matrix[1][2]   ='O'
-matrix[2][1]   ='O'
-matrix[2][2]   ='O'
-matrix[11][2]  ='X'
-matrix[12][0]  ='X'
-matrix[12][2]  ='X'
-matrix[13][1]  ='X'
-matrix[13][2]  ='X'
-matrix[7][5]   ='X'
-matrix[8][5]   ='X'
-matrix[8][7]   ='X'
-matrix[9][5]   ='X'
-matrix[9][6]   ='X'
-matrix[33][2]  ='X'
-matrix[33][3]  ='X'
-matrix[33][4]  ='X'
-matrix[32][4]  ='X'
-matrix[33][3]  ='X'
-matrix[15][14] ='X'
-matrix[14][14] ='X'
-matrix[13][14] ='X'
-matrix[12][14] ='X'
-matrix[11][14] ='X'
-matrix[10][14] ='X'
-matrix[24][2]  ='O'
-matrix[24][3]  ='O'
-matrix[24][4]  ='O'
-matrix[23][4]  ='O'
-matrix[22][3]  ='O'
-matrix[24][2]  ='O'
-matrix[24][3]  ='O'
-matrix[24][4]  ='O'
-matrix[23][4]  ='O'
-matrix[22][3]  ='O'
-matrix[41][2]  ='O'
-matrix[41][3]  ='O'
-matrix[41][4]  ='O'
-matrix[40][4]  ='O'
-matrix[39][3]  ='O'
-matrix[41][20]  ='X'
-matrix[41][21]  ='X'
-matrix[41][22]  ='X'
-matrix[40][22]  ='X'
-matrix[39][21]  ='X'
-
-matrix[40][0]  ='O'
-matrix[40][1]  ='O'
-matrix[40][4]  ='O'
-matrix[40][5]  ='O'
-matrix[40][6]  ='O'
-matrix[40][7]  ='O'
-matrix[40][8]  ='O'
-matrix[40][9]  ='O'
-matrix[40][12] ='O'
-matrix[40][13] ='O'
-matrix[40][14] ='O'
-matrix[40][16] ='O'
-matrix[40][17] ='O'
-matrix[40][18] ='O'
-matrix[40][19] ='O'
-
-
-
-
-
+from seed_matrix import R, C, MATRIX, create_matrix
 
 def wrap(r, c, R=R, C=C):
     return (r % R, c % C)
 
-def count_neighbors(r, c, matrix=matrix, R=R, C=C):
+def count_neighbors(r, c, matrix=MATRIX, R=R, C=C):
     xneighbors = 0
     yneighbors = 0
     sum_neighbors = 0
@@ -103,14 +30,14 @@ def generate_next_X(matrix):
             count = count_neighbors(row, cell, matrix)
             current = matrix[row][cell]
             
-            if current in ['X']: # HA! substitute `not in` for `in` here
+            if current in ['X']: 
                 if count['x'] < 2:
                     next[row][cell] = ' '
                 elif count['x'] in [2, 3]:
                     next[row][cell] = current
                 elif count['x'] < 3:
                     next[row][cell] = ' '
-            elif count['x'] == 3: # empty cell and exactly 3 neighbors
+            elif count['x'] == 3:
                 next[row][cell] = 'X'
             elif current in ['O']:
                 next[row][cell] = current
@@ -123,7 +50,7 @@ def generate_next_O(matrix):
             
             count = count_neighbors(row, cell, matrix)
             current = matrix[row][cell]
-            if current in ['O']: # HA! substitute `not in` for `in` here
+            if current in ['O']: 
                 if count['o'] < 2:
                     next[row][cell] = ' '
                 elif count['o'] in [2, 3]:
@@ -143,18 +70,19 @@ def show_matrix(matrix, count1, count2):
     
 count1 = 0
 count2 = 0
+matrix = MATRIX
 while 1:
     show_matrix(matrix, count1, count2)
     matrix = generate_next_X(matrix)
     count1 += 1
-    sleep(.04)
+    sleep(.5)
 
     show_matrix(matrix, count1, count2)
     matrix = generate_next_O(matrix)
     count2 += 1 
-    sleep(.04)
+    sleep(.5)
 
     show_matrix(matrix, count1, count2)
     matrix = generate_next_O(matrix)
     count2 += 1 
-    sleep(.04)
+    sleep(.5)
